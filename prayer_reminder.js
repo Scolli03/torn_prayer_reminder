6a0636b3f954d95ffe2032553fadd8108d351228
scolli03 <scolli03.sc@gmail.com>
2025-06-12 20:21:38 -0500

    Bump version to 2.5 for Torn City Prayer Reminder user script

--- prayer_reminder.js
+++ prayer_reminder.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Torn City Prayer Reminder
 // @namespace    http://tampermonkey.net/
-// @version      2.4
+// @version      2.5
 // @description  Reminds you to pray at the church in Torn City at configurable times (browser & Torn PDA). Supports manual times and auto interval snooze.
 // @author       YourName
 // @match        https://www.torn.com/*
